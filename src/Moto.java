2133e96cc0425cdae526639e34c777ef697af0fa
Alberto <alberto.spaziano@branchina.edu.it>
2025-05-21 10:26:48 +0200

    Added property in moto class

--- src/Moto.java
+++ src/Moto.java
@@ -1,4 +1,11 @@
 public class Moto implements Veicolo{
+    private String modello;
+    private int anno, cilindrata;
+    public Moto(String modello, int anno, int cilindrata) {
+        this.modello = modello;
+        this.anno = anno;
+        this.cilindrata = cilindrata;
+    }
     @Override
     public String getModello() {
         return "";
