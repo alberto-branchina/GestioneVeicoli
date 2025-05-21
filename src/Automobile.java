a682b4176afac9df61ae6baa2eb1bba576ce5940
Alberto <alberto.spaziano@branchina.edu.it>
2025-05-21 10:22:52 +0200

    Added Automobile.java

---
+++ src/Automobile.java
@@ -0,0 +1,27 @@
+public class Automobile implements Veicolo{
+    private int modello, anno, porte;
+
+    public Automobile(int modello, int anno, int porte) {
+        this.modello = modello;
+        this.anno = anno;
+        this.porte = porte;
+    }
+
+    @Override
+    public String getModello() {
+        return "";
+    }
+
+    @Override
+    public int getAnno() {
+        return 0;
+    }
+
+    @Override
+    public void StartMotore() {
+
+    }
+    public void startMotore(){
+        System.out.println("\uD83D\uDE97 Motore acceso: Brrrum!");
+    }
+}
